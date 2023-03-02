a8519c20a982aefa59a2535575b40d106524f7e5
Kariuki Joseph <kariuki.joseph121@gmail.com>
2023-03-02 17:19:13 +0300

    Debugging

---
+++ app/src/main/java/com/example/welfarecontribution/Dashboard.java
@@ -0,0 +1,32 @@
+package com.example.welfarecontribution;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.cardview.widget.CardView;
+
+import android.annotation.SuppressLint;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class Dashboard extends AppCompatActivity {
+
+    private CardView admin;
+    private CardView user;
+    @SuppressLint("MissingInflatedId")
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_dashboard);
+        admin = findViewById(R.id.admin);
+        user = findViewById(R.id.user);
+        admin.setOnClickListener(View->{
+            Intent intent = new Intent(Dashboard.this, AdminLogin.class);
+            startActivity(intent);
+            finish();
+        });
+
+        user.setOnClickListener(View->{
+            Intent intent = new Intent(Dashboard.this, UserLogin.class);
+            startActivity(intent);
+            finish();
+        });
+    }
+}
